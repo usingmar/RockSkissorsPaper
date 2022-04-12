d51ee924d63603957f97f4f9e2a90027ec7d199a
usingmar <60043029+usingmar@users.noreply.github.com>
2022-04-12 23:30:22 +0300

    Update RulesLogic.js

--- RulesLogic.js
+++ RulesLogic.js
@@ -5,14 +5,6 @@ class RulesLogic{
         this.mooves = mooves_;
     }
 
-    setMooves(mooves_){
-        this.mooves = mooves_;
-    }
-
-    getResult(){
-        return this.result;
-    }
-
     calculateResult(userMoove, computerMoove){
         if(userMoove === computerMoove) return 'Draw';
         if(userMoove > computerMoove) return userMoove-computerMoove > Math.floor(this.mooves.length/2) ? 'Win' : 'Loose';
